a037ccb3bf5e790f548f58907b3bb0466dba1d4c
Swifter <swifter@Swifterui-Mac.local>
2019-12-04 08:28:31 +0900

    @HELLO

--- install.sh
+++ install.sh
@@ -5,29 +5,10 @@
 git config --global user.name "LEE SEGUN"git config --global user.email "segunleedev@gmail.com"
 
 
-# install brew
-if ! which brew
-then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-read -r -s -p "[sudo] sudo password for $(whoami):" pass
-
-# install via brew
-brew bundle --file=./Brewfile
-
-# install font
-cp -a ./fonts/. ~/Library/Fonts
-
 
 # configure zsh
 chmod 755 ./zsh/install.sh
 ./zsh/install.sh
-
-
-# install xcode
-chmod 755 ./xcode/install.sh
-./xcode/install.sh
 
 
 # echo "$pass" | sudo -S xattr -dr com.apple.quarantine /Applications/Google\ Chrome.app
