a8c4aed7cc1869f0cf9af37cbce6f50c95e9a170
LEE SEGUN <segunleedev@gmail.com>
2019-12-04 09:14:56 +0900

    @HELL

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,16 +4,14 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 
-# copy my zsh settings
-cp ./zsh/.zshrc ~/.zshrc
-# cp ./zsh/.p10k.zsh ~/.p10k.zsh
+brew install nodejs
 
 
-# install zsh theme
+# install zsh theme (1)
 # git clone https://github.com/romkatv/powerlevel10k.git /Users/$USER/.oh-my-zsh/themes/powerlevel10k
 
 
-# install zsh theme
+# install zsh theme (2)
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 fpath+=("$HOME/.zsh/pure")
 
@@ -24,3 +22,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /Users/$USER/
 
 # zsh-autosuggestionsgit 
 git clone git://github.com/zsh-users/zsh-autosuggestions /Users/$USER/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+
+
+# copy my zsh settings
+cp ./zsh/.zshrc ~/.zshrc
+# cp ./zsh/.p10k.zsh ~/.p10k.zsh
